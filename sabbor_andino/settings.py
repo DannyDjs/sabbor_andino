8bddc92e9f12529fb542b3cf8464c07e967c2980
DannyDjs <143233013+DannyDjs@users.noreply.github.com>
2024-09-27 21:54:15 -0500

    Update settings.py

--- sabbor_andino/settings.py
+++ sabbor_andino/settings.py
@@ -135,7 +135,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'app', 'static', 'app')
+    os.path.join(BASE_DIR, 't_gastronomico', 'static', 't_gastronomico')
 ]
 
 MEDIA_URL = '/media/'
